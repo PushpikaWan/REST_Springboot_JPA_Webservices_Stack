f53c75cfd08475ae506d6ea4cb32d5322a20b07a
PushpikaWan <lucefer41@gmail.com>
2020-02-04 18:29:56 +0530

    feat: initial commit of generated spring boot project of SOAP web services

---
+++ soap-course/README.md
@@ -0,0 +1,5 @@
+# SOAP Web services
+- in spring, typically it uses contract first approach (define format of the request and response before development)
+- 
+
+##
---
+++ soap-course/src/test/java/com/spring/basic/soap/webservice/soapcourse/SoapCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.basic.soap.webservice.soapcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
